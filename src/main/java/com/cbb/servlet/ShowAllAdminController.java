c3df68cfedf330c5661ffb9290cc7844f2bc4211
cbb <470557347@qq.com>
2022-06-10 10:46:36 +0800

    Separate business logic and page presentation

--- src/main/java/com/cbb/servlet/ShowAllAdminController.java
+++ src/main/java/com/cbb/servlet/ShowAllAdminController.java
@@ -10,14 +10,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
-@WebServlet(value = "/showall")
-public class ShowAllAdminServlet extends HttpServlet {
+@WebServlet(value = "/showallcontroller")
+public class ShowAllAdminController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 只负责调用业务逻辑功能
+        AdminService adminService = new AdminServiceImpl();
 
+        List<Admin> adminList = adminService.showAllAdmin();
+
+        // request作用域存储
+        req.setAttribute("admins", adminList);
+        // 转发跳转到显示结果servlet
+        req.getRequestDispatcher("/showalljsp").forward(req, resp);
     }
 
     @Override
